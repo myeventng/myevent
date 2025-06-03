453592b01ee531e9ccd2a2b3de585c85a4df2562
Calstins <myevents.com.ng@gmail.com>
2025-06-03 11:02:20 +0100

    deployment fix4 @typescript-eslint/no-unused-vars off3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,12 @@ export default [
     files: ['src/generated/prisma/**/*'],
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
     },
   },
 
