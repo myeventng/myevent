ccb51b06ed89df93480f308fe87c8c2d636f3c1b
Calstins <myevents.com.ng@gmail.com>
2025-06-03 10:50:35 +0100

    deployment fix4 @typescript-eslint/no-unused-vars off2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,20 +9,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
+  // Extend Next.js rules
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
+  // Ignore Prisma generated files
   {
-    // This object defines your custom rule overrides
+    ignores: ['src/generated/prisma/**'],
+  },
+
+  // Override rules for Prisma generated files
+  {
+    files: ['src/generated/prisma/**/*'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+
+  // Your custom global rule overrides
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      // 'no-unused-vars': 'off',
-      // 'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-      // 'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-      // 'import/no-unresolved': 'off', // Disable unresolved import errors
     },
   },
 ];
-
-export default eslintConfig;
