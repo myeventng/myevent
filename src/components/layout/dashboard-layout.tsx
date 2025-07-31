d58be24bea5167468cc6f75441b97f3a15b072c9
Calstins <myevents.com.ng@gmail.com>
2025-07-31 16:49:39 +0100

    added notification

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import { Sidebar } from './sidebar';
 import { Header } from './header';
 import { AuthSession } from '@/lib/auth-client';
-// import { NotificationProvider } from '@/context/use-notification-context';
-// import { NotificationSetup } from '@/components/notification/notification-setup';
-// import { NotificationToast } from '@/components/notification/notificatio-toast';
+import { NotificationProvider } from '@/context/use-notification-context';
+import { NotificationSetup } from '@/components/notification/notification-setup';
+import { NotificationToast } from '@/components/notification/notificatio-toast';
 interface DashboardLayoutProps {
   children: React.ReactNode;
   session: AuthSession;
@@ -17,31 +17,31 @@ export function DashboardLayout({ children, session }: DashboardLayoutProps) {
 
   return (
     <div className="flex h-screen overflow-hidden">
-      {/* <NotificationProvider>
+      <NotificationProvider>
         <NotificationSetup />
-        <NotificationToast /> */}
+        <NotificationToast />
 
-      {/* Sidebar */}
-      <Sidebar
-        user={session.user}
-        isOpen={sidebarOpen}
-        collapsed={collapsed}
-        onCollapseToggle={() => setCollapsed(!collapsed)}
-        onMobileClose={() => setSidebarOpen(false)}
-      />
-
-      {/* Main content */}
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Header
+        {/* Sidebar */}
+        <Sidebar
           user={session.user}
-          onMobileToggle={() => setSidebarOpen(!sidebarOpen)}
+          isOpen={sidebarOpen}
+          collapsed={collapsed}
+          onCollapseToggle={() => setCollapsed(!collapsed)}
+          onMobileClose={() => setSidebarOpen(false)}
         />
 
-        <main className="flex-1 overflow-y-auto bg-gray-50 p-4 md:p-6">
-          {children}
-        </main>
-      </div>
-      {/* </NotificationProvider> */}
+        {/* Main content */}
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Header
+            user={session.user}
+            onMobileToggle={() => setSidebarOpen(!sidebarOpen)}
+          />
+
+          <main className="flex-1 overflow-y-auto bg-gray-50 p-4 md:p-6">
+            {children}
+          </main>
+        </div>
+      </NotificationProvider>
     </div>
   );
 }
